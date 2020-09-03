0d885bb8b87ff9dbb9e283e5df67d20e8299db2f
Marcin Polak <mapoart@gmail.com>
2020-09-03 10:56:58 +0200

    Linux and MacOS configs added.

--- dart.linux.nexss.config.js
+++ dart.linux.nexss.config.js
@@ -1,8 +1,8 @@
 let languageConfig = Object.assign({}, require("./dart.win32.nexss.config"));
+const installDartScript = `${__dirname}/install/setupChannel.linux.sh`;
 languageConfig.compilers = {
   dart: {
-    install:
-      "sudo apt-add-repository ppa:dartsim/ppa && sudo apt-get update && sudo apt-get install libdart6-all-dev",
+    install: `bash ${installDartScript}`,
     command: "dart",
     args: "<file>",
     help: ``,
